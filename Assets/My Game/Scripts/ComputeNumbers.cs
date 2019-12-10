819550bcae84607fabd48bd180fd3b769ea4581c
kevinbulay <kevinbulay@gmx.at>
2019-12-10 16:44:08 +0100

    changed Inputfield variables name from var A/B to inputField1/2

--- Assets/My Game/Scripts/ComputeNumbers.cs
+++ Assets/My Game/Scripts/ComputeNumbers.cs
@@ -4,33 +4,33 @@ using UnityEngine.UI;
 public class ComputeNumbers : MonoBehaviour
 {
     public Text result;
-    public InputField varA;
-    public InputField varB;
+    public InputField inputField1;
+    public InputField inputField2;
     public Button btn_reset;
     public Button btn_add_Numbers;
 
     public void SetResult()
     {
         result.text = AddNumbers().ToString();
-        varA.interactable = false;
-        varB.interactable = false;
+        inputField1.interactable = false;
+        inputField2.interactable = false;
         btn_add_Numbers.interactable = false;
         btn_reset.interactable = true;
     }
 
     public void Reset()
     {
-        varA.text = "0";
-        varB.text = "0";
+        inputField1.text = "0";
+        inputField2.text = "0";
         varA.interactable = true;
-        varB.interactable = true;
+        inputField2.interactable = true;
         btn_add_Numbers.interactable = true;
         btn_reset.interactable = false;
         result.text = "Result";
     }
-    private int AddNumbers()
+    private float AddNumbers()
     {
-        int tempResult = int.Parse(varA.text) + int.Parse(varB.text);
+        float tempResult = float.Parse(inputField1.text) + float.Parse(inputField2.text);
         return tempResult;
     }
 }
